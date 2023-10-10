c07450cf0668fffa2245fbea5226d9a9a8b46726
divan <hlinakcurbag@gmail.com>
2023-10-10 10:42:41 +0300

    задания 4

---
+++ python_tasks_easy/task_4_1.py
@@ -0,0 +1,13 @@
+# Введите 2 строки с клавиатуры - объедините их и подсчитайте количество любого символа.
+
+
+string1 = input("Введите первую строку: ")
+string2 = input("Введите вторую строку: ")
+
+combined_string = string1 + string2
+
+char_to_count = input("Введите символ для подсчета: ")
+
+count = combined_string.count(char_to_count)
+
+print(f"Количество символа '{char_to_count}' в объединенной строке равно {count}")
---
+++ python_tasks_easy/task_4_3.py
@@ -0,0 +1,7 @@
+# Введите строку с клавиатуры, переведите её в нижний регист и замените все буквы "а" на "е".
+
+user_input = input("Введите строку: ")
+
+modified_string = user_input.lower().replace('а', 'е')
+
+print(f"Измененная строка: {modified_string}")
